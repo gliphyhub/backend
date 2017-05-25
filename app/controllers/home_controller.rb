4e062bd79447c0d269d6825d7e1729179261c06e
Willie <wmag360@gmail.com>
2017-05-24 19:47:59 -0500

    Se me pasó el archivo de publicaciones, está en el home_controller :'v

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,4 +19,62 @@ class HomeController < ApplicationController
   	return "gliphymain" if action_name == "bienvenida"
   	super
   end
+  private
+    def insertar_grupos
+      grupos = params[:grupos]#recibimos los grupos en un arreglo
+      publicacion_id = params[:publicacion_id]#recibimos el id de la publicacion (debe existir)
+      publicacion = Publicacion.find(publicacion_id)#buscamos la publicación con su id
+      grupos.each do |w|#empezamos a llenar los registros una por una
+        id = Grupo.select("id").where(grupo: w)#obtenemos el id del grupo
+        grupo = Grupo.find(id)#buscamos el grupo con su id
+        nuevo = PublicacionGrupo.new({
+          grupo: grupo,
+          publicacion: publicacion
+        })#creamos el registro
+        if nuevo.save()
+          #procesos que siguen despues de guardados
+        else
+          #procesos que siguen si no se guardaron
+        end
+      end
+    end
+    #Cargar las publicaciones
+    #######Para el controlador########
+    #def 
+      #@archi = Archivo.select("id, nombre, ruta, publicacion_id")
+      #@archivos = Dir.entries(Ruta_directorio_archivos)
+      #@urgentes = Publicacion.select("titulo, mensaje, fecha_de_termino").where(prioridad: true, condicion de que la fecha actual sea menor a la de termino)
+      #@publicaciones = Publicacion.select("titulo, mensaje, fecha_de_termino").where(prioridad: false, condicion de que la fecha actual sea menor a la de termino)
+    #end
+    #######Para el haml#########
+    #-@urgentes.each do |w|
+      #%li
+        #%h3
+          #= w.titulo
+        #%p
+          #- @archi.each do |m| 
+            #- if m.publicacion_id == w.id
+              #- @archivos.each do |a| 
+                #-if a == m.nombre
+                  #- if m == "." || m == ".." then next end
+                  #%strong=a
+                  #%br
+                  #%a{:target => "_blank", :href => "ruta_del_archivo/#{a}"}
+                    #Descargar archivo
+                  #%br
+    #-@publicaciones.each do |w|
+      #%li
+        #%h3
+          #= w.titulo
+        #%p
+          #- @archi.each do |m| 
+            #- if m.publicacion_id == w.id
+              #- @archivos.each do |a| 
+                #-if a == m.nombre
+                  #- if m == "." || m == ".." then next end
+                  #%strong=a
+                  #%br
+                  #%a{:target => "_blank", :href => "ruta_del_archivo/#{a}"}
+                    #Descargar archivo
+                  #%br
 end
